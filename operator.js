631a1b0f2090472b9a4b25a5fb8abf223bf8b203
masudalakib <masudalakib@gmail.com>
2023-08-10 23:15:40 +0600

    o

--- operator.js
+++ operator.js
@@ -24,7 +24,21 @@
 // print (Fahrenheit 94 Degree)
 // print Tast complete in console
 
-var temp = prompt("Please enter a Temperature: ");
-var result = 9/5*temp + 32;
-alert("Rahrenheit: " + result + "Degree");
-console.log("Task Complete!");
+// var temp = prompt("Please enter a Temperature: ");
+// var result = 9/5*temp + 32;
+// alert("Rahrenheit: " + result + "Degree");
+// console.log("Task Complete!");
+
+
+// **************
+let nae = "Akib";
+let age = "44";
+let dob = "21 June, 2000";
+
+console.log(`His name is ${nae}
+ HIs age is ${age} 
+ Date of Birth ${dob}`);
+
+let a = 21
+let b = 10
+console.log(`${a}+ ${b}= ${a + b}`);
