f83a0cab9aa25656d8901a572e53a76a0154f02e
masudalakib <masudalakib@gmail.com>
2023-08-17 23:58:54 +0600

    function

---
+++ function.js
@@ -0,0 +1,16 @@
+//user Defined Function
+
+
+//Declartion of function
+function saySomething(){
+    console.log("Hello");
+    console.log("Learning Js");
+    console.log("Done");
+}
+
+
+//Excution of Function
+
+saySomething();
+// saySomething();
+// saySomething();
